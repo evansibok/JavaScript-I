310fc41a1b5dea859645a38bba9e49ee67ad142c
Evans Ibok <evansibok@gmail.com>
2019-08-20 00:40:21 +0100

    completed challenge 6 stretch in arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -132,11 +132,13 @@ console.log(oldCars.length);
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only 
 // contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-// BMWAndAudi = inventory.filter((car) => car.car_make === 'BMW' || car.car_make === 'Audi');
-// console.log(JSON.stringify(BMWAndAudi));
 for (let i = 0; i < inventory.length; i++) {
   if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
     BMWAndAudi.push(inventory[i]);
   }
 }
+console.log(JSON.stringify(BMWAndAudi));
+
+// Challenge 6 Stretch
+BMWAndAudi = inventory.filter((car) => car.car_make === 'BMW' || car.car_make === 'Audi');
 console.log(JSON.stringify(BMWAndAudi));
